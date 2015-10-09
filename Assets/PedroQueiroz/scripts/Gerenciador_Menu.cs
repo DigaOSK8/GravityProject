489d2e9b9aa605bdab207a3f6c3656d4a328c58b
Lukasz Swiderski <lukasz@swiderski.xyz>
2015-10-09 19:16:10 +0100

    Character selected integrated with the level1

---
+++ Assets/PedroQueiroz/scripts/Gerenciador_Menu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gerenciador_Menu : MonoBehaviour {
+
+    public GameObject inicial;
+    public GameObject credits;
+
+	// Use this for initialization
+	void Start () {
+        inicial.SetActive(true);
+    }
+
+    #region Credits
+
+    public void btn_Credits_Back()
+    {
+        inicial.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    #endregion
+
+    #region Inicial
+
+    public void btn_Inicial_PlayGame()
+    {
+        credits.SetActive(false);
+        inicial.SetActive(false);
+    }
+
+    public void btn_Inial_Credits()
+    {
+        credits.SetActive(true);
+        inicial.SetActive(false);
+    }
+
+    public void btn_Inicial_Exit()
+    {
+        Application.Quit();
+    }
+
+    #endregion
+
+
+
+
+
+}
--- Assets/Scripts/CharacterSelectController.cs
+++ Assets/Scripts/CharacterSelectController.cs
@@ -12,6 +12,7 @@ public class CharacterSelectController : MonoBehaviour
     private string axisX;
     private KeyCode buttonA;
     private float axisInput = 0.0f;
+    private int selectedByPlayer1 = 0;
 
 
     // Use this for initialization
@@ -34,6 +35,7 @@ public class CharacterSelectController : MonoBehaviour
             CharactersColor = Instantiate(CharactersColor);
             Destroy(CharactersBW);
             ((ScreenSwitcher)ScreenSwitcher.GetComponent(typeof(ScreenSwitcher))).Play();
+            Destroy(this);
         }
         axisInput = Input.GetAxis(axisX);
 
@@ -41,20 +43,55 @@ public class CharacterSelectController : MonoBehaviour
         {
             MoveToLeft(TriangleP1);
             MoveToRight(TriangleP2);
+            selectedByPlayer1 = 0;
+
+            var players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (var player in players)
+            {
+                var script = player.GetComponent<PlayerController>() as PlayerController;
+                if (player.name == "Player 1")
+                {
+                    script.SetPlayerAs1();
+                }
+                else if(player.name == "Player 2")
+                {
+                    script.SetPlayerAs2();
+                }
+            }
         }
         else if(axisInput > 0.1f)
         {
             MoveToLeft(TriangleP2);
             MoveToRight(TriangleP1);
+            selectedByPlayer1 = 1;
+            var players = GameObject.FindGameObjectsWithTag("Player");
+            foreach (var player in players)
+            {
+                var script = player.GetComponent<PlayerController>() as PlayerController;
+                if (player.name == "Player 1")
+                {
+                    script.SetPlayerAs2();
+                }
+                else if (player.name == "Player 2")
+                {
+                    script.SetPlayerAs1();
+                }
+            }
         }
         axisInput = 0.0f;
     }
     private void MoveToRight(GameObject player)
     {
+        //set player to right position
         player.transform.position = new Vector3(5.5f, 32f, 85);
     }
     private void MoveToLeft(GameObject player)
     {
+        //set player to left position
         player.transform.position = new Vector3(-5.5f, 32f, 85);
+    }
+    public int GetSelectResultByPlayer1()
+    {
+        return selectedByPlayer1; // return 0 when left and 1 when right
     }
 }
